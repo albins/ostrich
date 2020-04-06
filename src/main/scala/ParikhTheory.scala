b5a5b26caffd880daf8aa24ae69db55cbb1c1d9d
Albin Stjerna <albin.stjerna@gmail.com>
2020-04-06 10:39:17 +0200

    Flow equations in the theory formulation

--- src/main/scala/ParikhTheory.scala
+++ src/main/scala/ParikhTheory.scala
@@ -1,12 +1,21 @@
 package strsolver
 import ap.theories._
 import ap.parser._
+import ap.basetypes.IdealInt
 import preprop.BricsAutomaton
 import ap.terfor.conjunctions.Conjunction
-import ap.terfor.{TermOrder}
+import ap.terfor.{TermOrder, Term, Formula}
+import ap.terfor.TerForConvenience._
+import ap.terfor.linearcombination.LinearCombination
+import strsolver.preprop.EdgeWrapper._
 
 class ParikhTheory(private val aut: BricsAutomaton) extends Theory {
-  import IExpression._
+  import IExpression.Predicate
+
+  def trace[T](message: String)(something: T): T = {
+    println(s"trace::${message}(${something})")
+    something
+  }
 
   // FIXME: total deterministisk ordning på edges!
   // FIXME: name the predicate!
@@ -18,15 +27,96 @@ class ParikhTheory(private val aut: BricsAutomaton) extends Theory {
   // register -> flow equations
   // domänvillkor
   override def preprocess(f: Conjunction, order: TermOrder): Conjunction = {
+    // val acceptingStatesArePositive = acceptingStateVar.values.toSeq >= 0
+    // val oneAcceptingStateUsed = acceptingStateVar.values.reduce(_ + _) === 1
+    implicit val newOrder = order
+
+    def asManyIncomingAsOutgoing(
+        transitionVars: Seq[LinearCombination]
+    ): Formula = {
+      def asStateFlowSum(
+          stateTerms: Seq[(aut.State, (IdealInt, LinearCombination))]
+      ) = {
+        val (state, _) = stateTerms.head
+        val isInitial =
+          (if (state == aut.initialState) LinearCombination.ONE
+           else LinearCombination.ZERO)
+
+        (state, sum(stateTerms.unzip._2 ++ List((IdealInt.ONE, isInitial))))
+      }
+
+      trace("Flow equations") {
+        conj(
+          aut.transitions
+            .zip(transitionVars.iterator)
+            .filter(!_._1.isSelfEdge)
+            .map {
+              case ((from, _, to), transitionVar) =>
+                List(
+                  (to, (IdealInt.MINUS_ONE, transitionVar)),
+                  (from, (IdealInt.ONE, transitionVar))
+                )
+            }
+            .flatten
+            .to
+            .groupBy(_._1)
+            .values
+            .map(asStateFlowSum _)
+            .map {
+              case (state, flowSum) =>
+                if (state.isAccept) flowSum >= 0 else flowSum === 0
+            }
+        )
+      }
+      // val (acceptingTransitionSums, nonAcceptingTransitionSums) = {
+      //   // FIXME This tuple unpacking is...not elegant
+      //   val (accept, nonAccept) = transitionSumsByState.partition(_._1.isAccept)
+      //   (accept.unzip._2, nonAccept.unzip._2)
+      // }
+
+      // // This expresses the constraint that at any transitions in and out from a
+      // // non-accepting state are balanced (as many incoming as outgoing
+      // // transitions).
+      // val nonAcceptingTransitionsBalanced = conj(
+      //   nonAcceptingTransitionSums.map(_ === 0)
+      // )
+
+      // // This expresses that the transitions in and out of the accepting states
+      // // allow ending up on precisely one accepting state.
+      // // val acceptingTransitionsBalanced = sum(
+      // //   Stream.continually(IdealInt.ONE) zip acceptingTransitionSums
+      // // ) === 1
+      // val acceptingTransitionsBalanced = conj(
+      //   acceptingTransitionSums.map(_ >= 0)
+      // )
+
+      // // This propagates a lower bound on the sums that ensures we do not have
+      // // "negative" paths.
+      // val acceptingTransitionsNotNegative = conj(
+      //   acceptingTransitionSums.map(_ >= 0)
+      // )
+
+      // trace("Flow equations") {
+      //   conj(
+      //     nonAcceptingTransitionsBalanced,
+      //     acceptingTransitionsBalanced,
+      //     acceptingTransitionsNotNegative
+      //   )
+      // }
+    }
+
     Theory.rewritePreds(f, order) { (atom, negated) =>
-      if (atom.pred == predicate) {
-        atom
+      if (atom.pred == this.predicate) {
+        Conjunction.conj(
+          List(atom, asManyIncomingAsOutgoing(atom.to[Seq])),
+          order
+        )
       } else atom
     }
   }
 
   // BEGIN USELESS BOILERPLATE
-  val axioms: ap.terfor.Formula = Conjunction.TRUE
+  val axioms: Formula = Conjunction.TRUE
   val functionPredicateMapping
       : Seq[(ap.parser.IFunction, ap.parser.IExpression.Predicate)] = List()
   val functionalPredicates: Set[ap.parser.IExpression.Predicate] = Set()
@@ -38,7 +128,6 @@ class ParikhTheory(private val aut: BricsAutomaton) extends Theory {
   // END USELESS BOILERPLATE
 
   // TODO propagator för connectedness
-  // TODO ta också BORT predikat när vi är klara med dem
   def plugin: Option[ap.proof.theoryPlugins.Plugin] = None
 
   /**
@@ -47,7 +136,8 @@ class ParikhTheory(private val aut: BricsAutomaton) extends Theory {
     *
     **/
   def allowsRegisterValues(registerValues: Seq[ITerm]): IFormula = {
-    val transitionTermSorts = List.fill(aut.edges.size)(Sort.Integer)
+    import IExpression._
+    val transitionTermSorts = List.fill(aut.edges.size)(Sort.Integer) //
     val transitionTerms = (0 until aut.edges.size).map(v)
     ex(
       transitionTermSorts,
